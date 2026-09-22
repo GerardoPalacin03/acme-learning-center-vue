cc8f6299d672728c03c6016a9be432273677f688
Gerardo Palacin <gerardopalacin.github@gmail.com>
2026-09-22 18:45:57 -0500

    feat(about): add about component with translation feature

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
 
-const routes = [];
+const about =()=> import("./shared/presentation/views/about.vue");
+const routes= [
+    { path: "/home", name: "home", component: Home, meta: { title: "Home" } },
+    {path: "/about", name: "about", component: about, meta: { title: "About Us" } }
+];
+
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
