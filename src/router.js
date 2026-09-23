f1f9ee6599b86c997ff1545013763a1ab8fb3b09
Gerardo Palacin <gerardopalacin.github@gmail.com>
2026-09-22 19:05:20 -0500

    feat(page-not-found): add page not found component with translation feature

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./shared/presentation/views/home.vue";
 
+const pageNotFound =()=> import("./shared/presentation/views/page-not-found.vue");
 const about =()=> import("./shared/presentation/views/about.vue");
+
 const routes= [
     { path: "/home", name: "home", component: Home, meta: { title: "Home" } },
-    {path: "/about", name: "about", component: about, meta: { title: "About Us" } }
+    {path: "/about", name: "about", component: about, meta: { title: "About Us" }},
+    {path:'/',redirect:'/home'},
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page Not Found' } }
+
 ];
 
 
